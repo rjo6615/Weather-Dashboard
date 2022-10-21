bc6038afeb206453316a5695a33d52c3c41e69c6
rjo6615 <robertjobernier@gmail.com>
2022-10-21 15:59:47 -0400

    todays weather now displays on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var units = "units=imperial";
 var APIKey = "appid=ce9cf9f240e5f73b22d7ec2394c97ed6";
 var dailyWeather = "https://api.openweathermap.org/data/2.5/weather?q=";
 var forecastWeather ="https://api.openweathermap.org/data/2.5/onecall?";
+var weatherIcon = "http://openweathermap.org/img/wn/";
 
 // Element Variables
 var citySearchForm = $("#citySearchForm");
@@ -15,12 +16,22 @@ var getWeather = function () {
     event.preventDefault();
     var dailyWeatherApiUrl = dailyWeather + searchCity + "&" + APIKey + "&" + units;
     fetch(dailyWeatherApiUrl).then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      })};
-
+        if (response.ok) {
+        return response.json().then(function (response) {
+            $("#cityName").html(response.name);
+            var date = moment.unix(response.dt).format("MM/DD/YY");
+            $("#currentDate").html(date);
+            var weatherIcon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+            $("#weatherIcon").attr("src", weatherIcon);
+            $("#temp").html(response.main.temp + "\u00B0F");
+            $("#wind").html(response.wind.speed + "MPH");
+            $("#humidity").html(response.main.humidity + "%");            
+    });
+    }else {
+        alert("Enter a city name");    
+    }
+});
+};
 
 $(citySearchForm).on("submit", function () {
    searchCity = $("#searchCity").val().trim();
